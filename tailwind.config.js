0d76877712d48357a33833eeb459b0cea88aeb2f
Anil Khairnar <anilrkhairnar@gmail.com>
2022-09-07 17:37:33 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        golden: "#ad9970",
+        grey: "#767676",
+        whiteGrey: "#ffffff80",
+        darkGrey: "#00000080",
+        ghostWhite: "#f8f9fa",
+        raisinBlack: "#262626",
+        red: "#ff6347",
+      },
+      screens: {
+        xl: "1200px",
+      },
+    },
+  },
+  plugins: [],
+};
